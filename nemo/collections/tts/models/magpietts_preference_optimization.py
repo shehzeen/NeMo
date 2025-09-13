86421b192faa8cf7d96095ab58063d01e2e0c795
Shehzeen Hussain <shehzeensh@gmail.com>
2025-09-12 23:16:56 -0700

    handle unsupported languages for the normalizer
    Signed-off-by: Shehzeen Hussain <shehzeensh@gmail.com>

--- nemo/collections/tts/models/magpietts_preference_optimization.py
+++ nemo/collections/tts/models/magpietts_preference_optimization.py
@@ -80,7 +80,13 @@ class MagpieTTSModelOfflinePODataGen(MagpieTTSModel):
         lang_key = lang_key if lang_key else "en"
         if lang_key not in self._normalizer_cache:
             logging.info(f"Creating normalizer for language: {lang_key}")
-            self._normalizer_cache[lang_key] = Normalizer(input_case="cased", lang=lang_key)
+            try:
+                self._normalizer_cache[lang_key] = Normalizer(input_case="cased", lang=lang_key)
+            except NotImplementedError:
+                logging.warning(
+                    f"Text normalization for language {lang_key} is not implemented. Will not normalize this language."
+                )
+                self._normalizer_cache[lang_key] = None
         return self._normalizer_cache[lang_key]
 
     def test_step(self, batch, batch_idx):
