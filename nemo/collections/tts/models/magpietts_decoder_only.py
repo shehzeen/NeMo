324b8038f103ba9839489dd8ef6d1fcb9cd3361c
paarthneekhara <paarthneekhara@users.noreply.github.com>
2026-01-11 08:14:21 +0000

    Apply isort and black reformatting
    Signed-off-by: paarthneekhara <paarthneekhara@users.noreply.github.com>

--- nemo/collections/tts/models/magpietts_decoder_only.py
+++ nemo/collections/tts/models/magpietts_decoder_only.py
@@ -171,15 +171,24 @@ class MagpieTTSDecoderModel(ModelPT):
 
         if cfg.transformer_hf_backend == "custom_qwen3_moe_5layer":
             from transformers.models import qwen3_moe
-            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(hidden_size=1536, intermediate_size=3072, num_hidden_layers=5, num_experts=64)
+
+            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(
+                hidden_size=1536, intermediate_size=3072, num_hidden_layers=5, num_experts=64
+            )
             self.decoder = qwen3_moe.modeling_qwen3_moe.Qwen3MoeModel(config)
         elif cfg.transformer_hf_backend == "custom_qwen3_moe_10layer":
             from transformers.models import qwen3_moe
-            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(hidden_size=1536, intermediate_size=3072, num_hidden_layers=10, num_experts=64)
+
+            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(
+                hidden_size=1536, intermediate_size=3072, num_hidden_layers=10, num_experts=64
+            )
             self.decoder = qwen3_moe.modeling_qwen3_moe.Qwen3MoeModel(config)
         elif cfg.transformer_hf_backend == "custom_qwen3_moe_15layer":
             from transformers.models import qwen3_moe
-            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(hidden_size=1536, intermediate_size=3072, num_hidden_layers=15, num_experts=64)
+
+            config = qwen3_moe.configuration_qwen3_moe.Qwen3MoeConfig(
+                hidden_size=1536, intermediate_size=3072, num_hidden_layers=15, num_experts=64
+            )
             self.decoder = qwen3_moe.modeling_qwen3_moe.Qwen3MoeModel(config)
             # from transformers.models import qwen2_moe
             # config_qwen2 = qwen2_moe.configuration_qwen2_moe.Qwen2MoeConfig(
