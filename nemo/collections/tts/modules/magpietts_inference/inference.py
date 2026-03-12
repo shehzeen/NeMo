9d95ed4a87b7c3c489183615ecf196fc8e84918c
Paarth Neekhara <paarth.n@gmail.com>
2026-03-12 17:22:57 -0400

    bug fixed
    Signed-off-by: Paarth Neekhara <paarth.n@gmail.com>

--- nemo/collections/tts/modules/magpietts_inference/inference.py
+++ nemo/collections/tts/modules/magpietts_inference/inference.py
@@ -581,7 +581,7 @@ class EasyMagpieInferenceRunner(BaseInferenceRunner):
             pad_context_text_to_max_duration=False,
             context_duration_min=context_duration_min,
             context_duration_max=context_duration_max,
-            ignore_phoneme_languages=self.config.get('ignore_phoneme_languages', []),
+            ignore_phoneme_languages=self.model.cfg.get('ignore_phoneme_languages', []),
             add_language_to_context_text=self.model.add_language_to_context_text,
         )
         dataset.text_tokenizer = self.model.tokenizer
